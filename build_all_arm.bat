80c071f8fbbd992e30d63752bcb1a042137329e3
Russ Butler <russ.butler@arm.com>
2017-01-30 17:07:17 -0600

    Use ARM linker script in codebase

--- build_all_arm.bat
+++ build_all_arm.bat
@@ -1,4 +1,4 @@
-mbed compile -j0 -t ARM -m K64F --build BUILD_BOOTLOADER/K64F/ARM -DENTRY_POINT=main_bootloader -DMAIN_ADDR=0x10000 -l custom\MK64FN1M0xxx12_bootloader.sct
-mbed compile -j0 -t ARM -m K64F --build BUILD_MAIN//K64F/ARM -DMAIN_ADDR=0x10000 -l custom\MK64FN1M0xxx12_application.sct
+mbed compile -j0 -t ARM -m K64F --profile custom/default_bootloader.json --build BUILD_BOOTLOADER/K64F/ARM -DENTRY_POINT=main_bootloader -DMAIN_ADDR=0x10000
+mbed compile -j0 -t ARM -m K64F --profile custom/default_application.json --build BUILD_MAIN//K64F/ARM -DMAIN_ADDR=0x10000
 python combine.py mbed-os-example-blinky.bin --app "BUILD_BOOTLOADER/K64F/ARM/mbed-os-example-blinky.bin,0x00000000" --app "BUILD_MAIN/K64F/ARM/mbed-os-example-blinky.bin,0x00010000"
 
